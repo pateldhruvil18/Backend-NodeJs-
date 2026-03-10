9cd19a0fb76719182e99d651df63b2b5833da84e
pateldhruvil18 <DHP204600@GMAIL.COM>
2026-03-10 14:58:34 +0530

    add email functionality

--- FileUpload/models/File.js
+++ FileUpload/models/File.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const nodemailer = require("nodemailer");
 const fileSchema = new mongoose.Schema({
     name: {
         type:String,
@@ -14,6 +14,29 @@ const fileSchema = new mongoose.Schema({
     email : {
         type: String,
     },
+})
+
+//post midlleware
+fileSchema.post("save", async function(doc) {
+    try {
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+        let info = await transporter.sendMail({
+            from:'Dhruvil Patel',
+            to: doc.email,
+            subject: "new file uploaded cloudinary",
+            html:`<h2>hello JE</h2> <p>File uploaded</p> View here <a href="${doc.imageUrl}></a>`
+        })
+
+        console.log(info)
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 const File = mongoose.model("File", fileSchema);
